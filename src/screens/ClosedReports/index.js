371f02c77b5a54cb042993a4720573b5a3e8ef52
Dodycode <prasetyodody17@gmail.com>
2019-12-12 15:29:43 +0800

    first setup

---
+++ src/screens/ClosedReports/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+class ClosedReports extends React.Component{
+ render(){
+  return(
+   <View>
+   		<Text>Hello report!</Text>
+   </View>
+  );
+ }
+}
+
+export default ClosedReports;
