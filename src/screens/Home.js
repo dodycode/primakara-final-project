517080f18f2b6ec0e7c0252baba15fd6d11a1d5e
Widhiarsa Sujiwa <42883798+Jiwmeister@users.noreply.github.com>
2019-12-17 12:38:30 +0800

    Revert "first setup"

---
+++ src/screens/Home.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react';
+import { View, Text } from 'react-native';
+const Home = () => {
+	return(
+		<View>
+			<Text>Home...</Text>
+		</View>
+	)
+}
+export default Home;
