555000ac6471f24ee09e3813579a4c555aa4b685
Dodycode <prasetyodody17@gmail.com>
2020-01-08 18:50:33 +0800

    finishing proceed and reject feature

--- src/screens/QueuedReports/index.js
+++ src/screens/QueuedReports/index.js
@@ -12,31 +12,110 @@ class QueuedReports extends React.Component {
         this.state = {
             reports: null,
             loading: false,
-            showMenu: false
+            showMenu: false,
+            currentSelectedId: null,
+            currentSelectedAction: null
         }
     }
 
-    toggleMenu = () => {
+    hideMenu = () => {
         this.setState({
-            showMenu: !this.state.showMenu
+            showMenu: false
         })
+    }
+
+    showMenu = id => {
+        this.setState({
+            showMenu: id
+        });
+    }
+
+    confirmAlert = async (msg, func, id) => {
+        await this.setState({
+            currentSelectedId: id,
+            currentSelectedAction: func
+        });
+        Alert.alert(
+          'Confirmation Message',
+          msg,
+          [
+            {
+              text: 'Cancel',
+              style: 'cancel',
+            },
+            {
+                text: 'OK', 
+                onPress: () => {
+                    switch(this.state.currentSelectedAction) {
+                        case 1:
+                            this.setProceedReports()
+                            break;
+                        case 2:
+                            this.rejectReports()
+                            break;
+                    }
+                }
+            },
+          ],
+          {cancelable: false},
+        );
+    }
+
+    setProceedReports = async () => {
+        if (this.state.currentSelectedId != null) {
+            this.setState({
+                loading: true
+            });
+            await firestore()
+            .collection('reports')
+            .doc(this.state.currentSelectedId)
+            .update({
+                status: 'In Process'
+            })
+            .then(async report => {
+                await this.getReports();
+            })
+            .catch(err => {
+                this.setState({
+                    loading: false
+                });
+                Alert.alert('Error', err);
+            });
+        }
+    }
+
+    rejectReports = async () => {
+        if (this.state.currentSelectedId != null) {
+            this.setState({
+                loading: true
+            });
+            await firestore()
+            .collection('reports')
+            .doc(this.state.currentSelectedId)
+            .delete()
+            .then(async report => {
+                await this.getReports();
+            })
+            .catch(err => {
+                this.setState({
+                    loading: false
+                });
+                Alert.alert('Error', err);
+            });
+        }
     }
 
     getReports = async () => {
         const querySnapshot = await firestore()
         .collection('reports')
-        .where('status', '==', 'Queued');
-
-        this.setState({
-            loading: true
-        });
-
-        querySnapshot.onSnapshot({
-            error: e => Alert.alert('Error', e),
-            next: reports => {
+        .where('status', '==', 'Queued')
+        .get()
+        .then(snapshot => {
+            if (snapshot._docs.length > 0) {
                 const list = [];
-
-                reports.forEach(async report => {
+                snapshot
+                .docs
+                .forEach(async report => {
                     const {title, desc, imgSrc, date} = report.data();
                     await list.push({
                         id: report.id,
@@ -46,17 +125,58 @@ class QueuedReports extends React.Component {
                         date: date
                     });
                 });
-
                 this.setState({
                     loading: false,
                     reports: list
+                });
+            }else{
+                this.setState({
+                    loading: false,
+                    reports: null
                 });
             }
         })
     }
 
     componentDidMount() {
-        this.getReports();
+        this.setState({
+            loading: true
+        });
+        this.unsubscribe = firestore()
+        .collection('reports')
+        .where('status', '==', 'Queued')
+        .onSnapshot({
+            error: e => Alert.alert('Error', e),
+            next: reports => {
+                if (reports._docs.length > 0) {
+                    const list = [];
+
+                    reports.forEach(async report => {
+                        const {title, desc, imgSrc, date} = report.data();
+                        await list.push({
+                            id: report.id,
+                            title: title,
+                            desc: desc,
+                            imgSrc: imgSrc,
+                            date: date
+                        });
+                    });
+
+                    this.setState({
+                        loading: false,
+                        reports: list
+                    });
+                }else{
+                    this.setState({
+                        loading: false
+                    });
+                }
+            }
+        });
+    }
+
+    componentWillUnmount() {
+        this.unsubscribe();
     }
 
     render() {
@@ -78,11 +198,11 @@ class QueuedReports extends React.Component {
                                                 <Caption>Arif Muhammad - {data.date}</Caption>
                                             </View>
                                             <Menu
-                                                visible={this.state.showMenu}
-                                                onDismiss={this.toggleMenu}
+                                                visible={this.state.showMenu == data.id}
+                                                onDismiss={this.hideMenu}
                                                 anchor={
                                                     <TouchableRipple
-                                                    onPress={this.toggleMenu}>
+                                                    onPress={() => this.showMenu(data.id)}>
                                                         <Image
                                                         style={{width: 14, height: 14, resizeMode: 'contain'}} 
                                                         source={require('../../assets/menu.png')}></Image>
@@ -93,8 +213,20 @@ class QueuedReports extends React.Component {
                                                 Alert.alert(data.title, data.desc)
                                             }} title="View" />
                                             <Divider />
-                                            <Menu.Item title="Proceed" />
-                                            <Menu.Item title="Reject" />
+                                            <Menu.Item 
+                                            title="Proceed" 
+                                            onPress={() => this.confirmAlert(
+                                                'Yakin ingin memproses laporan ini??',
+                                                1,
+                                                data.id
+                                            )} />
+                                            <Menu.Item 
+                                            title="Reject"
+                                            onPress={() => this.confirmAlert(
+                                                'Yakin ingin menolak laporan ini??',
+                                                2,
+                                                data.id
+                                            )} />
                                             </Menu>
                                         </Card.Content>
                                     </Card>
@@ -102,7 +234,7 @@ class QueuedReports extends React.Component {
                                 </View>
                             );
                         })
-                    ) : null}
+                    ) : <Caption style={{alignSelf: 'center', marginTop: 14}}>Tidak ada laporan.</Caption>}
                 </ScrollView>
             </React.Fragment>
         );
